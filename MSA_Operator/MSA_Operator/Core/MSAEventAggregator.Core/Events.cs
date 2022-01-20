0accf8f6df46f1e6a6b56bc5471aa07d5a5a0e7c
Filip Mystek <filip.mystek@gmail.com>
2022-01-20 09:25:54 +0100

    New events to communicate between modules

--- MSA_Operator/MSA_Operator/Core/MSAEventAggregator.Core/Events.cs
+++ MSA_Operator/MSA_Operator/Core/MSAEventAggregator.Core/Events.cs
@@ -12,4 +12,11 @@ namespace MSAEventAggregator.Core
     public class MapDetailEvent : PubSubEvent<object>{}
     public class AnimateOverlay : PubSubEvent<bool> {}
     public class AddPin : PubSubEvent<bool> {}
+   // public class AddPinToMapEvent : PubSubEvent<bool> {}
+    public class LogoutEvent : PubSubEvent<bool> {}
+    public class CameraWindowEvent : PubSubEvent<bool> {}
+    public class LocalizationFindEvent : PubSubEvent<bool> {}
+
+    public class CenterLocationChange : PubSubEvent<string> { };
+    public class CloseLocalizationDetails : PubSubEvent { };
 }

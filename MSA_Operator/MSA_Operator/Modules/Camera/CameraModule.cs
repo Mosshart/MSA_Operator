10abd599bb634af470ff794834f00b3424ffeeb0
Filip Mystek <filip.mystek@gmail.com>
2022-02-16 20:54:01 +0100

    Removed old project, old rosdll, and other obsolete files, chenged ui file name to UI_prototype

--- MSA_Operator/MSA_Operator/Modules/Camera/CameraModule.cs
+++ MSA_Operator/MSA_Operator/Modules/Camera/CameraModule.cs
@@ -1,5 +1,4 @@
-﻿using Camera.Views;
-using Prism.Ioc;
+﻿using Prism.Ioc;
 using Prism.Modularity;
 using Prism.Regions;
 
--- MSA_Operator_Old/MSA_Operator/ViewModels/MapPinViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿using Prism.Mvvm;
-
-namespace MSA_Operator.ViewModels
-{
-    public class MapPinViewModel : BindableBase
-    {
-    }
-}
